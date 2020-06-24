c48e4b2e7583a807e1c984c2f8e9ce7583e3b39a
Anushka Raj <anush6633@gmail.com>
2020-06-24 21:28:54 +0530

    Add files via upload

---
+++ Python/study_5/page6/script.py
@@ -0,0 +1,15 @@
+from food import Food
+from drink import Drink
+
+food1 = Food('Sandwich', 5)
+food1.calorie_count = 330
+print(food1.info())
+
+# Create an instance of the Drink class and assign it to the drink1 variable
+
+
+# Set the volume variable of drink1 to 180
+
+
+# Call the info method from drink1 and output the return value
+
